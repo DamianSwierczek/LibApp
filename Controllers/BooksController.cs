09fa826ce9ffa6c5d2e73da3b76d83c433f79d26
Świerczek  Damian <damian.swierczek@microsoft.wsei.edu.pl>
2021-10-16 16:10:07 +0200

    Added ways of passing data to views

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -15,7 +15,10 @@ namespace LibApp_Gr3.Controllers
             {
                 var firstBook = new Book() { Name = "English dictionary" };
 
-                return View(firstBook);
+            //ViewData["Book"] = firstBook;
+            ViewBag.Book = firstBook;
+
+                return View();
             }
 
         public IActionResult Edit(int bookId)
